7f74f69551d206cdc6f5f4dfbe6e7b2787db9998
Vishnu Bharathi <vishnubharathi04@gmail.com>
2025-09-19 15:44:44 +0530

    feat: add association for product domain (#14)
    * feat: add association for product domain
    
    * try domain association
    
    * improve formatting
    
    * format before exiting
    
    * improve formatting
    
    * add more tests
    
    * use continue on error
    
    * adjust gh test
    
    * adjust the test
    
    * adjust the logging

--- upload-file/main.go
+++ upload-file/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"strings"
 
 	"github.com/hasura/security-agent-tools/upload-file/input"
 	"github.com/hasura/security-agent-tools/upload-file/metadata"
@@ -41,7 +42,21 @@ func main() {
 		if err != nil {
 			log.Fatalf("Failed to associate image name with scan: %v", err)
 		}
-		log.Printf("Associated image name %s with scan %s\n", imageName, scan.ID)
+		log.Printf("Associated image name: %s\n", imageName)
+	}
+
+	domain := input.Tags["product_domain"]
+	if domain != "" {
+		err = metadata.AssociateProductDomainWithScan(context.Background(), c, scan.ID, domain)
+		if err != nil {
+			pd, _ := metadata.ProductDomains(context.Background(), c)
+			var pds strings.Builder
+			for _, p := range pd {
+				pds.WriteString("  - " + p + "\n")
+			}
+			log.Fatalf("Failed to associate product domain with scan: %v. Please check `product_domain` value is one of the following:\n%s", err, pds.String())
+		}
+		log.Printf("Associated product domain: %s\n", domain)
 	}
 
 	err = upload.ServiceMetadata(context.Background(), c, input)
